72cfb8c0e0beb08f204e0969e603675a0f185200
saathvikc <83232359+saathvikc@users.noreply.github.com>
2023-08-14 20:45:49 -0400

    fixed sign in/sign out

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,12 @@
 import Head from "next/head";
 import Link from "next/link";
 import { api } from "~/utils/api";
-import { SignIn } from "@clerk/nextjs"; 
-import { SignInButton } from "@clerk/nextjs";
+import { SignIn, SignInButton, SignOutButton, useUser } from "@clerk/nextjs"; 
 
 export default function Home() {
   const hello = api.example.hello.useQuery({ text: "from tRPC" });
+  
+  const user = useUser();
 
   return (
     <>
@@ -15,7 +16,10 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
-        <SignInButton />
+        <div>
+          {!user.isSignedIn && <SignInButton />}
+          {!!user.isSignedIn && <SignOutButton />}
+        </div>
         <SignIn path="/sign-in" routing="path" signUpUrl="/sign-up" />
       </main>
     </>
