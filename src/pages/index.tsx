b2a524abedd1f6ed633359934565d480370595ae
saathvikc <83232359+saathvikc@users.noreply.github.com>
2023-08-14 22:53:59 -0400

    updated routers

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,9 +4,11 @@ import { api } from "~/utils/api";
 import { SignIn, SignInButton, SignOutButton, useUser } from "@clerk/nextjs"; 
 
 export default function Home() {
-  const hello = api.example.hello.useQuery({ text: "from tRPC" });
+  // const hello = api.example.hello.useQuery({ text: "from tRPC" });
   
   const user = useUser();
+
+  const {data} = api.posts.getAll.useQuery();
 
   return (
     <>
@@ -20,7 +22,9 @@ export default function Home() {
           {!user.isSignedIn && <SignInButton />}
           {!!user.isSignedIn && <SignOutButton />}
         </div>
-        <SignIn path="/sign-in" routing="path" signUpUrl="/sign-up" />
+        <div>
+          {data?.map((post) => (<div key={post.id}>{post.content}</div>))}
+        </div>
       </main>
     </>
   );
