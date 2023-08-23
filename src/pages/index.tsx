c0472fc5673a8c47b93b7043620271ef182c604d
saathvikc <saathvik.chandupatla@gmail.com>
2023-08-23 16:53:50 -0400

    added components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -215,24 +215,84 @@ export default function Home() {
                   <div className="border-b pb-12">
                     <h2 className="text-3xl font-semibold leading-7 text-gray-200 pl-8">Industry Experience</h2>
                     <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6 pl-8">
+
+                      <div className="sm:col-span-1">
+                        <label htmlFor="years-within-industry" className="block text-sm font-medium leading-6 text-gray-200">
+                          Years Within Industry
+                        </label>
+                        <div className="mt-2">
+                          <input
+                            type="number"
+                            name="years-within-industry"
+                            id="years-within-industry"
+                            className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                          />
+                        </div>
+                      </div>
+
+                      <div className="sm:col-span-1">
+                        <label htmlFor="clinical-trials-number" className="block text-sm font-medium leading-6 text-gray-200">
+                          Number of Clinical Trials Participated In
+                        </label>
+                        <div className="mt-2">
+                          <input
+                            type="number"
+                            name="clinical-trials-number"
+                            id="clinical-trials-number"
+                            className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                          />
+                        </div>
+                      </div>
+
+                      <div className="sm:col-span-1">
+                        <label htmlFor="organizations-number" className="block text-sm font-medium leading-6 text-gray-200">
+                          Number of Organizations
+                        </label>
+                        <div className="mt-2">
+                          <input
+                            type="number"
+                            name="organizations-number"
+                            id="organizations-number"
+                            className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                          />
+                        </div>
+                      </div>
+
                       <div className="sm:col-span-3">
-                          <label htmlFor="specialty" className="block text-sm font-medium leading-6 text-gray-200">
-                            Therapautic Specialty
+                        <label htmlFor="specialty" className="block text-sm font-medium leading-6 text-gray-200">
+                          Therapautic Specialty
+                        </label>
+                        <div className="mt-2">
+                          <select
+                            id="specialty"
+                            name="specialty"
+                            autoComplete="specialty"
+                            className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6"
+                          >
+                            <option>Pediatrics</option>
+                            <option>Oncology</option>
+                            <option>Cardiology</option>
+                            <option>Dermatology</option>
+                          </select>
+                        </div>
+                      </div>
+                      
+                      <fieldset>
+                        <legend className="sr-only">Select Levels of Education</legend>
+                        <div className="mb-[0.125rem] block min-h-[1.5rem] pl-[1.5rem]">
+                          <input
+                            className="relative float-left -ml-[1.5rem] mr-[6px] mt-[0.15rem] h-[1.125rem] w-[1.125rem] appearance-none rounded-[0.25rem] border-[0.125rem] border-solid border-neutral-300 outline-none before:pointer-events-none before:absolute before:h-[0.875rem] before:w-[0.875rem] before:scale-0 before:rounded-full before:bg-transparent before:opacity-0 before:shadow-[0px_0px_0px_13px_transparent] before:content-[''] checked:border-primary checked:bg-primary checked:before:opacity-[0.16] checked:after:absolute checked:after:-mt-px checked:after:ml-[0.25rem] checked:after:block checked:after:h-[0.8125rem] checked:after:w-[0.375rem] checked:after:rotate-45 checked:after:border-[0.125rem] checked:after:border-l-0 checked:after:border-t-0 checked:after:border-solid checked:after:border-white checked:after:bg-transparent checked:after:content-[''] hover:cursor-pointer hover:before:opacity-[0.04] hover:before:shadow-[0px_0px_0px_13px_rgba(0,0,0,0.6)] focus:shadow-none focus:transition-[border-color_0.2s] focus:before:scale-100 focus:before:opacity-[0.12] focus:before:shadow-[0px_0px_0px_13px_rgba(0,0,0,0.6)] focus:before:transition-[box-shadow_0.2s,transform_0.2s] focus:after:absolute focus:after:z-[1] focus:after:block focus:after:h-[0.875rem] focus:after:w-[0.875rem] focus:after:rounded-[0.125rem] focus:after:content-[''] checked:focus:before:scale-100 checked:focus:before:shadow-[0px_0px_0px_13px_#3b71ca] checked:focus:before:transition-[box-shadow_0.2s,transform_0.2s] checked:focus:after:-mt-px checked:focus:after:ml-[0.25rem] checked:focus:after:h-[0.8125rem] checked:focus:after:w-[0.375rem] checked:focus:after:rotate-45 checked:focus:after:rounded-none checked:focus:after:border-[0.125rem] checked:focus:after:border-l-0 checked:focus:after:border-t-0 checked:focus:after:border-solid checked:focus:after:border-white checked:focus:after:bg-transparent dark:border-neutral-600 dark:checked:border-primary dark:checked:bg-primary dark:focus:before:shadow-[0px_0px_0px_13px_rgba(255,255,255,0.4)] dark:checked:focus:before:shadow-[0px_0px_0px_13px_#3b71ca]"
+                            type="checkbox"
+                            value=""
+                            id="checkboxDefault" />
+                          <label
+                            className="inline-block pl-[0.15rem] hover:cursor-pointer"
+                            htmlFor="checkboxDefault">
+                            Default checkbox
                           </label>
-                          <div className="mt-2">
-                            <select
-                              id="specialty"
-                              name="specialty"
-                              autoComplete="specialty"
-                              className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6"
-                            >
-                              <option>Pediatrics</option>
-                              <option>Oncology</option>
-                              <option>Cardiology</option>
-                              <option>Dermatology</option>
-                            </select>
-                          </div>
-                      </div>  
+                        </div>
+                      </fieldset>  
+                          
                     </div>
                   </div>
                   
