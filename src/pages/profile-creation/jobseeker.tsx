b8a667b0412f4b4bff3f8f1de2b83316ba3072e2
saathvikc <saathvik.chandupatla@gmail.com>
2023-08-24 11:43:11 -0400

    added routing

---
+++ src/pages/profile-creation/jobseeker.tsx
@@ -0,0 +1,249 @@
+import Head from "next/head";
+import Link from "next/link";
+import { api } from "~/utils/api";
+import { SignInButton, SignOutButton, useUser } from "@clerk/nextjs"; 
+import { PhotoIcon, UserCircleIcon } from '@heroicons/react/24/solid'
+
+export default function jobseeker() {
+
+return (
+    <form>
+        <div className="space-y-12">
+            <div className="border-b pb-12">
+            <h2 className="text-3xl font-semibold leading-7 text-gray-200 pl-8 pt-8">Personal Information</h2>
+            {/* <p className="mt-1 text-sm leading-6 text-gray-600">Use a permanent address where you can receive mail.</p> */}
+
+            <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6 pl-8 pr-8">
+                <div className="sm:col-span-3">
+                <label htmlFor="first-name" className="block text-sm font-medium leading-6 text-gray-200">
+                    First name
+                </label>
+                <div className="mt-2">
+                    <input
+                    type="text"
+                    name="first-name"
+                    id="first-name"
+                    autoComplete="given-name"
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    />
+                </div>
+                </div>
+
+                <div className="sm:col-span-3">
+                <label htmlFor="last-name" className="block text-sm font-medium leading-6 text-gray-200">
+                    Last name
+                </label>
+                <div className="mt-2">
+                    <input
+                    type="text"
+                    name="last-name"
+                    id="last-name"
+                    autoComplete="family-name"
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-200 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    />
+                </div>
+                </div>
+
+                <div className="col-span-full">
+                <label htmlFor="photo" className="block text-sm font-medium leading-6 text-gray-200">
+                    Photo
+                </label>
+                <div className="mt-2 flex items-center gap-x-3">
+                    <UserCircleIcon className="h-12 w-12 text-gray-200" aria-hidden="true" />
+                    <button
+                    type="button"
+                    className="rounded-md bg-white px-2.5 py-1.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+                    >
+                    Change
+                    </button>
+                </div>
+                </div>
+
+                <div className="sm:col-span-4">
+                <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-200">
+                    Email address
+                </label>
+                <div className="mt-2">
+                    <input
+                    id="email"
+                    name="email"
+                    type="email"
+                    autoComplete="email"
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-200 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    />
+                </div>
+                </div>
+
+                <div className="sm:col-span-3">
+                <label htmlFor="country" className="block text-sm font-medium leading-6 text-gray-200">
+                    Country
+                </label>
+                <div className="mt-2">
+                    <select
+                    id="country"
+                    name="country"
+                    autoComplete="country-name"
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6"
+                    >
+                    <option>United States</option>
+                    <option>Canada</option>
+                    <option>Mexico</option>
+                    </select>
+                </div>
+                </div>
+
+                <div className="col-span-full">
+                <label htmlFor="street-address" className="block text-sm font-medium leading-6 text-gray-200">
+                    Street address
+                </label>
+                <div className="mt-2">
+                    <input
+                    type="text"
+                    name="street-address"
+                    id="street-address"
+                    autoComplete="street-address"
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-200 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    />
+                </div>
+                </div>
+
+                <div className="sm:col-span-2 sm:col-start-1">
+                <label htmlFor="city" className="block text-sm font-medium leading-6 text-gray-200">
+                    City
+                </label>
+                <div className="mt-2">
+                    <input
+                    type="text"
+                    name="city"
+                    id="city"
+                    autoComplete="address-level2"
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-200 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    />
+                </div>
+                </div>
+
+                <div className="sm:col-span-2">
+                <label htmlFor="region" className="block text-sm font-medium leading-6 text-gray-200">
+                    State / Province
+                </label>
+                <div className="mt-2">
+                    <input
+                    type="text"
+                    name="region"
+                    id="region"
+                    autoComplete="address-level1"
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-200 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    />
+                </div>
+                </div>
+
+                <div className="sm:col-span-2">
+                <label htmlFor="postal-code" className="block text-sm font-medium leading-6 text-gray-200">
+                    ZIP / Postal code
+                </label>
+                <div className="mt-2">
+                    <input
+                    type="text"
+                    name="postal-code"
+                    id="postal-code"
+                    autoComplete="postal-code"
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-200 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    />
+                </div>
+                </div>
+            </div>
+            </div>
+
+            <div className="border-b pb-12">
+            <h2 className="text-3xl font-semibold leading-7 text-gray-200 pl-8">Industry Experience</h2>
+            <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6 pl-8">
+
+                <div className="sm:col-span-1">
+                <label htmlFor="years-within-industry" className="block text-sm font-medium leading-6 text-gray-200">
+                    Years Within Industry
+                </label>
+                <div className="mt-2">
+                    <input
+                    type="number"
+                    name="years-within-industry"
+                    id="years-within-industry"
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    />
+                </div>
+                </div>
+
+                <div className="sm:col-span-1">
+                <label htmlFor="clinical-trials-number" className="block text-sm font-medium leading-6 text-gray-200">
+                    Number of Clinical Trials Participated In
+                </label>
+                <div className="mt-2">
+                    <input
+                    type="number"
+                    name="clinical-trials-number"
+                    id="clinical-trials-number"
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    />
+                </div>
+                </div>
+
+                <div className="sm:col-span-1">
+                <label htmlFor="organizations-number" className="block text-sm font-medium leading-6 text-gray-200">
+                    Number of Organizations
+                </label>
+                <div className="mt-2">
+                    <input
+                    type="number"
+                    name="organizations-number"
+                    id="organizations-number"
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    />
+                </div>
+                </div>
+
+                <div className="sm:col-span-3">
+                <label htmlFor="specialty" className="block text-sm font-medium leading-6 text-gray-200">
+                    Therapautic Specialty
+                </label>
+                <div className="mt-2">
+                    <select
+                    id="specialty"
+                    name="specialty"
+                    autoComplete="specialty"
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6"
+                    >
+                    <option>Pediatrics</option>
+                    <option>Oncology</option>
+                    <option>Cardiology</option>
+                    <option>Dermatology</option>
+                    </select>
+                </div>
+                </div>
+                
+                <fieldset>
+                <legend className="sr-only">Select Levels of Education</legend>
+                
+                <div className="relative flex items-start">
+                    <div className="flex h-6 items-center">
+                    <input
+                        id="comments"
+                        aria-describedby="comments-description"
+                        name="comments"
+                        type="checkbox"
+                        className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600"
+                    />
+                    </div>
+                    <div className="ml-3 text-sm leading-6">
+                    <label htmlFor="comments" className="font-medium text-gray-200">
+                        New comments
+                    </label>
+                    </div>
+                </div>
+                                
+                </fieldset>  
+                    
+            </div>
+            </div>
+            
+        </div>
+    </form>
+)}
