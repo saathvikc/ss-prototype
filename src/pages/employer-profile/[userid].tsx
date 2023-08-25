bd7b56d7b1ce263eda91e338b320141c2846b74a
saathvikc <saathvik.chandupatla@gmail.com>
2023-08-25 11:05:30 -0400

    added beginning of profile

---
+++ src/pages/employer-profile/[userid].tsx
@@ -0,0 +1,10 @@
+import Head from "next/head";
+import Link from "next/link";
+import { api } from "~/utils/api";
+
+export default function EmployerProfile() {
+
+    return (
+        <h1>Employer Profile View</h1>
+    );
+}
---
+++ src/pages/jobseeker-profile/[userid].tsx
@@ -0,0 +1,10 @@
+import Head from "next/head";
+import Link from "next/link";
+import { api } from "~/utils/api";
+
+export default function JobSeekerProfile() {
+
+    return (
+        <h1>Job Seeker Profile View</h1>
+    );
+}
